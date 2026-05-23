6ecf7c6acfb0035dadfc4dfc964e8b5e27ed8a98
Abhronil Dutta <177771087+Abhronil-Dutta@users.noreply.github.com>
2026-05-23 16:06:51 -0700

    halloc v1 is done, hfree remaining and testing after that

--- halloc.c
+++ halloc.c
@@ -1,18 +1,89 @@
 #include "halloc.h"
 
-void* halloc(size_t size){
-    printf("%p\n", sbrk(0));
-    void* start = sbrk((int)size + sizeof(Header));
-    printf("%p\n", sbrk(0));
-
-    Header* header = (Header*)start;
-    header->free = 0;
-    header->size = size;
-    header->next = NULL;
-
-    void* user_ptr = (void*)((char*)start + sizeof(Header));
-    return user_ptr;
+Header* find_free_block(size_t size){
+    Header* current = heap_start;
+    while (current != NULL){
+        if (current->free == 1 && current->size >= size){
+            return current;
+        }
+        current = (Header*)current->next;
+    }
+    return NULL;
 }
+
+Header* expand_heap(size_t size) {
+    size_t needer = size + sizeof(Header);
+    void* new_space = sbrk(needed);
+    if (new_space == (void*)-1){
+        return NULL;
+    }
+
+    Header* last = heap_start;
+    while (last->next != NULL) {
+        last = (Header*)last->next;
+    }
+
+    Header* new_block = (Header*)new_space;
+    new_block->free = 1;
+    new_block->size = needed - sizeof(Header);
+    new_block->next = NULL;
+    new_block->prev = last;
+    
+    last->next = new_block;
+    heap_end = (char*)new_space + needed;
+    
+    return new_block;
+}
+
+void allocate_block(Header* block, size_t size){
+    block->free = 0;
+
+    size_t remaining = block->size - size;
+
+    if (remaining > sizeof(Header)) {
+        Header* new_block = (Header*)((char*)block + sizeof(Header) + size);
+        
+        new_block->free = 1;
+        new_block->size = remaining - sizeof(Header);
+        new_block->next = block->next;
+        new_block->prev = (void*)block;
+        
+        if (block->next != NULL) {
+            ((Header*)block->next)->prev = (void*)new_block;
+        }
+        block->next = (void*)new_block;
+        block->size = size;
+    }
+}
+
+void* halloc(size_t size){
+    // CASE 1: First call innitial loop
+    if (heap_start == NULL){
+        void *innitial_heap = sbrk(1024);
+        if (innitial_heap == (void*)-1){
+            return NULL; // sbrk failed
+        }
+
+        heap_start = (Header*)innitial_heap;
+        heap_end = (char*)innitial_heap + 1024;
+
+        heap_start->free = 1;
+        heap_start->size = 1024 - sizeof(Header);
+        heap_start->next = NULL;
+        heap_start->prev = NULL;
+    }
+    Header* block = find_free_block(size);
+    if (block == NULL){
+        block = expand_heap(size);
+        if (block == NULL) return NULL; // sbrk failed
+    }
+
+    allocate_block(block, size)
+
+    return (void*)((char*)block + sizeof(Header));
+}
+
+
 
 int hfree(void* addr){
     Header* header = (Header*)((char*)addr - sizeof(Header));
@@ -22,13 +93,6 @@ int hfree(void* addr){
 int main(){
     void* ptr1 = halloc(100);
     printf("User got pointer: %p\n", ptr1);
-    
-    char* buf = (char*)ptr1;
-    buf[0] = 'A';
-    buf[1] = 'B';
-    printf("buf[0] = %c\n", buf[0]);
-    hfree(ptr1);
-    return 0;
 
 }
 
--- halloc.h
+++ halloc.h
@@ -4,14 +4,19 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void* halloc(size_t size);
-int hfree(void* addr);
-
 typedef struct {
     int free;     // 0 = not free; 1 = free
     size_t size;  // size of the usable block
     void* next;   // pointer to the next block
+    void* prev;   // pointer to the previous block
 } Header;  // 24 bytes
+
+static Header* heap_start = NULL;
+static void* heap_end = NULL;
+
+void* halloc(size_t size);
+int hfree(void* addr);
+
 
 #endif
 
